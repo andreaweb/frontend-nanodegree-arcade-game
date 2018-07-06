56901f84272ab39e312cb1af3d247a294f1917f2
andreaweb <deiainluv@gmail.com>
2018-07-06 14:44:33 -0300

    characters and bg rendered

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x || 100;
+    this.y = y || 100;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,12 +25,19 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var Player = function(x,y){
+    this.x = x || 0;
+    this.y = y || 0;
+    this.sprite = 'images/char-boy.png';
+};
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const enemy = new Enemy();
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
